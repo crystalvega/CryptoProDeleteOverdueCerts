16b5db0bc5acec58fd675103f5cfa3d57e3c801e
crystalvega <110161341+crystalvega@users.noreply.github.com>
2023-03-28 09:43:10 +0500

    Update
    Added lang 'RU' support
    Added Delete Container from FAT

--- CertMgr.py
+++ CertMgr.py
@@ -1,6 +1,6 @@
 from os import path, getcwd, chdir
 import PySimpleGUI as sg
-import subprocess, os
+import subprocess, os, shutil
 import win32security
 from winreg import *
 
@@ -13,7 +13,7 @@ def ParseCerts(text):
     if 'Subject' in text:
         CONFS = ['Subject', 'Not valid after', 'SHA1 Hash', 'Container']
     elif 'Субъект' in text:
-        CONFS = []
+        CONFS = ['Субъект', 'Истекает', 'SHA1 отпечаток', 'Контейнер']
     else:
         sg.Popup('Ошибка!\nВ системе не обнаружены ЭЦП.')
         exit()
@@ -39,11 +39,14 @@ def ParseCerts(text):
         if 'CN=' in returnvalue[i][0]:
             returnvalue[i][0] = returnvalue[i][0].split('CN=')[1]
         returnvalue[i][2] = returnvalue[i][2].split('  ')[0]
+        if len(returnvalue[i]) > 3:
+            if 'FAT12' in returnvalue[i][3]:
+                returnvalue[i][3] = returnvalue[i][3].split('\\')[2].replace('.000','')
     return returnvalue
 
 def GetCerts():
     global certp
-    certp, cspp = CheckCryptoPro()
+    certp = CheckCryptoPro()
     cwd = getcwd()
     try:
         chdir(certp)
@@ -58,15 +61,13 @@ def GetCerts():
 
 def CheckCryptoPro():
     if path.exists(r'C:\Program Files\Crypto Pro\CSP\csptest.exe'):
-        cspp = r'C:\Program Files\Crypto Pro\CSP\csptest.exe'
         certp = 'C:\Program Files\Crypto Pro\CSP'
     elif path.exists(r'C:\Program Files (x86)\Crypto Pro\CSP\csptest.exe'):
-        cspp = r'C:\Program Files (x86)\Crypto Pro\CSP\csptest.exe'
         certp = 'C:\Program Files (x86)\Crypto Pro\CSP'
     else:
         sg.Popup('Ошибка!\nПрограммное обеспечение Крипто ПРО не найдено.')
         exit()
-    return certp, cspp
+    return certp
 
 def FindCKInReg(nameck):
     try:
@@ -74,6 +75,12 @@ def FindCKInReg(nameck):
         CloseKey(rawKeyA)
         return "SOFTWARE\\WOW6432Node\\Crypto Pro\\Settings\\Users\\" + sid + "\\Keys\\" + nameck
     except:
+        return None
+    
+def FindCKInFat(disk, nameck):
+    if os.path.isdir(disk + ':\\' + nameck):
+        return disk + ':\\' + nameck
+    else:
         return None
 
 def FindClosedContainer(certs):
@@ -94,7 +101,20 @@ def FindClosedContainer(certs):
                     if con.startswith('\\\\.\\REGISTRY\\'):
                         dir_ck = FindCKInReg(con.replace('\\\\.\\REGISTRY\\',''))
                         datasaved.append('R')
+                    elif con.startswith('\\\\.\\FAT12'):
+                        dir_ck = FindCKInFat(con.replace('\\\\.\\FAT12_','').split('\\')[0], cert[3] + '.000')
+                        datasaved.append('F')
+                    else:
+                        dir_ck.append(None)
+                        datasaved.append('N')
                     returnvalue.append(dir_ck)
+                    break
+                elif con == 'OK.':
+                    returnvalue.append(None)
+                    datasaved.append('N')
+                    break
+
+                    
     for i in range(0,len(certs)):
         if len(certs[i]) > 3:
             certs[i][3] = returnvalue[i]
@@ -104,34 +124,38 @@ def FindClosedContainer(certs):
             certs[i].append(datasaved[i])
     return certs
 
-def DeleteCK(key1, key2=''):
-    
-    if key2=="":
-        currentkey = key1
-    else:
-        currentkey = key1+ "\\" +key2
-    
-    open_key = OpenKey(registry, key1 ,0,KEY_ALL_ACCESS)
-    infokey = QueryInfoKey(open_key)
-    for x in range(0, infokey[0]):
-        #NOTE:: This code is to delete the key and all subkeys.
-        #  If you just want to walk through them, then 
-        #  you should pass x to EnumKey. subkey = _winreg.EnumKey(open_key, x)
-        #  Deleting the subkey will change the SubKey count used by EnumKey. 
-        #  We must always pass 0 to EnumKey so we 
-        #  always get back the new first SubKey.
-        subkey = EnumKey(open_key, 0)
-        try:
-            DeleteKey(open_key, subkey)
-            print ("Removed %s\\%s " % ( key1, subkey))
-        except:
-            DeleteCK(registry, currentkey, subkey )
-            # no extra delete here since each call 
-            #to deleteSubkey will try to delete itself when its empty.
+def DeleteCKfromR(key1, key2=''):
+    if key1 != None:
+        if key2=="":
+            currentkey = key1
+        else:
+            currentkey = key1+ "\\" +key2
+        
+        open_key = OpenKey(registry, key1 ,0,KEY_ALL_ACCESS)
+        infokey = QueryInfoKey(open_key)
+        for x in range(0, infokey[0]):
+            subkey = EnumKey(open_key, 0)
+            try:
+                DeleteKey(open_key, subkey)
+                print ("Removed %s\\%s " % ( key1, subkey))
+            except:
+                DeleteCKfromR(registry, currentkey, subkey )
 
     DeleteKey(open_key,"")
     open_key.Close()
     print("Removed %s" % (currentkey))
+    
+def DeleteCKfromF(folder):
+    if folder != None:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                sg.Popup('Ошибка!\nНе удалось удалить "' + folder + '".')
 
 def Delete(certs):
     chdir(certp)
@@ -139,7 +163,9 @@ def Delete(certs):
     retvalue = True
     for cert in certs:
         if cert[3] == 'R':
-            DeleteCK(cert[2])
+            DeleteCKfromR(cert[2])
+        if cert[3] == 'F':
+            DeleteCKfromF(cert[2])
         ret = subprocess.run(['certmgr.exe', '-delete', '-thumbprint', cert[1]], shell=True, capture_output=True, text=True, encoding='866').returncode
         if ret != 0:
             certserror.append(cert)
--- Main.pyw
+++ Main.pyw
@@ -6,11 +6,15 @@ from command_runner.elevate import elevate
 def Main():
     certslist = CertMgr.GetCerts()
     certsfordelete = Action.GetOldCerts(certslist)
-    if Action.Confirm(certsfordelete):
-        e, certs = CertMgr.Delete(certsfordelete)
-        if not e:
-            Action.Error(certs)
-    print(1)
+    if len(certsfordelete) == 0:
+        Action.NotFound()
+    else:
+        if Action.Confirm(certsfordelete):
+            e, certs = CertMgr.Delete(certsfordelete)
+            if not e:
+                Action.Error(certs)
+            else:
+                Action.Success()
 
 if __name__ == "__main__":
     elevate(Main)
